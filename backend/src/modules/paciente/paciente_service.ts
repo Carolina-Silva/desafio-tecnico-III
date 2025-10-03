bba4600d3e5570151c4ae850dde6e2c792197878
Carolina-Silva <carol_paiva@outlook.com>
2025-10-02 22:26:08 -0300

    :wrench: chore: centraliza PrismaClient em lib

--- backend/src/modules/paciente/paciente_service.ts
+++ backend/src/modules/paciente/paciente_service.ts
@@ -1,7 +1,5 @@
-// src/modules/pacientes/paciente.service.ts
-import { PrismaClient, Prisma } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { Prisma } from '@prisma/client';
+import { prisma } from '../../lib/prisma';
 
 export class PacienteService {
   async create(data: Prisma.PacienteCreateInput) {
